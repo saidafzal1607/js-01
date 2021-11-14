1f7abd3b987b3d83a32a3bcc25fa081c7ea340d4
Saidafzal <said.dev1607@gmail.com>
2021-11-14 11:31:14 +0500

    add data types

---
+++ dist/index.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Variables - o'zgaruvchilar 
+// ma'lumot turlari -- data types 
+// primitive --asosiy
+//number, string, undefined, boolean , null, 
+// reference-array, object --- (function, null ,array)
+var myAge = 22;
+var myName = 'Saidafzal';
+var hasKids = true;
+var isMarried = false; //object  undefined dan tashqari haamasini o'z ichiga oladi
